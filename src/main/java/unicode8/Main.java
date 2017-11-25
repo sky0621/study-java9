8076c1d223eb46c31da2752639cb6ebc179191c1
sky0621 <sky14work@gmail.com>
2017-11-26 01:29:28 +0900

    unicode8

---
+++ src/main/java/unicode8/Main.java
@@ -0,0 +1,11 @@
+package unicode8;
+
+/**
+ * Created by SS on 2017/11/25.
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println(Character.getName(0x1F3C5));
+        System.out.println(Character.getName(0x1f37F));
+    }
+}
